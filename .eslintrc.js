4a3ea9ee20a98933e30daf0fabd140ca4c62ca72
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2019-12-01 21:31:49 +0900

    add vue-property-decorators and fix compile options

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,12 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
-    '@nuxtjs/eslint-config-typescript',
   ],
   plugins: ['prettier'],
   // add your custom rules here
--- components/Logo.vue
+++ components/Logo.vue
@@ -23,6 +23,12 @@
     </g>
   </svg>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Logo extends Vue {}
+</script>
 <style>
 .NuxtLogo {
   animation: 1s appear;
