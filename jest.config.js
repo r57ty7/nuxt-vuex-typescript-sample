e1444ac3287a68a28daa87586405132a48e3c228
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2019-12-02 22:58:25 +0900

    add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
 module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
   collectCoverage: true,
